8f03340153428e57341302a06758b9711019b4e1
Carson West <c7933552528@gmail.com>
2023-01-04 09:11:59 -0500

    i dont know what im doing

--- noted/index.js
+++ noted/index.js
@@ -2,7 +2,18 @@ var converter = new showdown.Converter();
 var on = true;
 
 var gen = function(text){
-    console.log(converter.makeHtml(text));
+    var start = '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width">\n<title>noted</title>\n<link href="./style.css" rel="stylesheet" type="text/css" />\n<link rel="icon" type="image/x-icon" href="https://thecarsonwest.github.io/jacob.ico">\n<script type="text/javascript" src="https://unpkg.com/showdown/dist/showdown.min.js"></script>\n<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/monokai-sublime.min.css">\n<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>\n<script>hljs.highlightAll();</script>\n<script src="index.js"></script>\n<link rel="stylesheet" href="./style.css">\n</head>\n<body><a href="../noted.html">Made with noted</a>'
+    var html = start.concat(converter.makeHtml(text)).concat('</body></html>')
+
+    
+        // Create a blob from the HTML string
+        var blob = new Blob([html], {type: 'text/html'});
+        
+        // Create a URL for the blob
+        var url = URL.createObjectURL(blob);
+        
+        // Open the URL in a new window
+        window.open(url);
 }
 
 var update = function(){
