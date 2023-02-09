59231470b0da0828787f9ec04a2e34387b4bd22f
Carson West <c7933552528@gmail.com>
2023-02-09 07:26:41 -0500

    color wheels!

--- noted/index.js
+++ noted/index.js
@@ -1,5 +1,14 @@
 var converter = new showdown.Converter();
 var on = true;
+var scheme = {
+    'bg' : '#222',
+    'h1' : '#009b77',
+    'h2' : '#aaa',
+    'h3' : '#aaa',
+    'hr' : '#d8d8d8',
+    'code' : '#d8ba7d',
+    'p' : '#aaa'
+}
 
 function downloadURL(url, name) {
     var link = document.createElement("a");
@@ -12,8 +21,9 @@ function downloadURL(url, name) {
   }
 
 var gen = function(text){
-    var start = '<!DOCTYPE html>\n<html>\n    <head>\n    <meta charset="utf-8">\n    <meta name="viewport" content="width=device-width">\n    <title>noted</title>\n    <style></style>\n    <link rel="icon" type="image/x-icon" href="https://thecarsonwest.github.io/jacob.ico">\n    <link rel="stylesheet" href="https://thecarsonwest.github.io/noted/monkai.css">    \n    <link rel="stylesheet" href="https://thecarsonwest.github.io/noted/style.css">\n</head>\n<body><a href="https://TheCarsonWest.github.io/noted.html">Made with noted</a>';
-    var html = start.concat(text.concat('</body></html>'));
+    var start = `<!DOCTYPE html>\n<html>\n    <head>\n<meta charset="utf-8">\n    <meta name="viewport" content="width=device-width">\n    <title>noted</title>\n    <link rel="icon" type="image/x-icon" href="https://thecarsonwest.github.io/jacob.ico">\n    <link rel="stylesheet" href="https://thecarsonwest.github.io/noted/monkai.css">    \n    <link rel="stylesheet" href="https://thecarsonwest.github.io/noted/style.css">\n</head>`;
+    var start = start.concat('\n<body><a href="https://TheCarsonWest.github.io/noted.html">Made with noted</a>');
+    var html = start.concat(text.concat(`<style>body {background: ${scheme['bg']};color: ${scheme['p']};}h1{color: ${scheme['h1']};}h2{color: ${scheme['h2']};}h3{color: ${scheme['h3']};}hr{background-color: ${scheme['hr']};}code{color: ${scheme['code']};}</style></body></html>`));
 
     
         // Create a blob from the HTML string
@@ -31,7 +41,7 @@ var update = function(){
     target = document.getElementById('preview'),
     converter = new showdown.Converter(),
     html = converter.makeHtml(text);
-    target.innerHTML = html;
+    target.innerHTML = `<style> .preview {background: ${scheme['bg']};color:${scheme['p']}} h1{color: ${scheme['h1']};} h2{color: ${scheme['h2']};}h3{color: ${scheme['h3']};}hr{background-color: ${scheme['hr']};}code{color: ${scheme['code']};}}</style>`.concat(html);
     
 }
 var toggle = function(target, btn, on, off){
@@ -40,3 +50,18 @@ var toggle = function(target, btn, on, off){
     examples.style.display = examples.style.display === 'none' ? 'block' : 'none';
     toggleButton.innerHTML = toggleButton.innerHTML === on ? off : on;
 };
+function defaults(){
+    scheme['h1']='#009b77';
+    scheme['bg']='#222222';
+    scheme['h2']='#aaa';
+    scheme['h3']='#aaa';
+    scheme['code']='#d8ba7d';
+    document.getElementById('bgColor').value = '#222222';
+    document.getElementById('h1Color').value = '#009b77';
+    document.getElementById('h2Color').value = '#aaaaaa';
+    document.getElementById('h3Color').value = '#aaaaaa';
+    document.getElementById('hrColor').value = '#d8d8d8';
+    document.getElementById('pColor').value = '#aaaaaa';
+    document.getElementById('codeColor').value = '#d8ba7d';
+    update()
+}
